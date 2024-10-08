c1f3f671c36ca42e23507937d279f580a4c0279d
victorcosmedev <victorcosme.profissional@gmail.com>
2024-10-08 19:59:12 -0300

    Estilização, utilização de spinner com fallBack e aplicação de imagens.

--- aula15/src/components/Cabecalho/Cabecalho.tsx
+++ aula15/src/components/Cabecalho/Cabecalho.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import Menu from "../Menu/Menu";
+import imgLogo from "@/img/logo.jpg";
 
 export default function Cabecalho() {
     return (
-        <header className="prose lg:prose-xl">
-            <h1>Cabecalho</h1>
+        <header className="cabecalho">
+            <Image src={imgLogo} alt='imagem do logo' height={100} width={100}/>
             <Menu />
         </header>
     )
