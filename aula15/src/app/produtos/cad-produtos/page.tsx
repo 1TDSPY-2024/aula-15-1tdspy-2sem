dd47f7177eebc78fc865b647dd852ab3a0ba2bfd
vitorvhsilva <dndragonbr@gmail.com>
2024-10-08 20:50:47 -0300

    Form de Cadastro finalizado

--- aula15/src/app/produtos/cad-produtos/page.tsx
+++ aula15/src/app/produtos/cad-produtos/page.tsx
@@ -1,4 +1,17 @@
+"use client"
+
+import { TipoProduto } from "@/types"
+import { useState } from "react"
+
 export default function CadProduto() {
+
+  const [produto, setProduto] = useState<TipoProduto>({
+    id: 0,
+    nome: "",
+    preco: 0.0,
+    qtd: 0
+  })
+
   return (
     <div>
       <h1>Home</h1>
@@ -6,56 +19,39 @@ export default function CadProduto() {
       <div>
         <form className="max-w-sm mx-auto">
           <div className="mb-5">
-            <label
-              form="email"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Your email
+            <label form="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Nome
             </label>
-            <input
-              type="email"
-              id="email"
+            <input type="text" id="idNome" name="nome" value={produto.nome} 
+              onChange={(e) => setProduto({...produto, nome: e.target.value})}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="name@flowbite.com"
-              required
-            />
+              placeholder="Nome do produto" required/>
           </div>
+
           <div className="mb-5">
-            <label
-              form="password"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Your password
+            <label form="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Preço
             </label>
-            <input
-              type="password"
-              id="password"
+            <input type="number" id="idPreco" name="preco"
+            onChange={(e) => setProduto({...produto, preco: parseFloat(e.target.value)})}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required
-            />
+              placeholder="Preço do produto" required/>
           </div>
-          <div className="flex items-start mb-5">
-            <div className="flex items-center h-5">
-              <input
-                id="remember"
-                type="checkbox"
-                value=""
-                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                required
-              />
-            </div>
-            <label
-              form="remember"
-              className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              Remember me
+
+          <div className="mb-5">
+            <label form="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Quantidade
             </label>
+            <input type="number" id="idQtd" name="qtd"
+            onChange={(e) => setProduto({...produto, preco: parseInt(e.target.value)})}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Quantidade do produto" required/>
           </div>
+
           <button
             type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Submit
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Cadastrar
           </button>
         </form>
       </div>
