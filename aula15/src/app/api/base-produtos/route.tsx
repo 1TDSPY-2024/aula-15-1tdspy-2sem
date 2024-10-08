2a60a2ee43ee01a740f43d4e7c3dc3f2edb5d13a
vitorvhsilva <dndragonbr@gmail.com>
2024-10-08 20:25:41 -0300

    Endpoint POST inplementado

--- aula15/src/app/api/base-produtos/route.tsx
+++ aula15/src/app/api/base-produtos/route.tsx
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import {promises as fs} from "fs";
+import { TipoProduto } from "@/types";
 
 export async function GET() {   
     const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
@@ -8,13 +9,19 @@ export async function GET() {
 }
 
 export async function POST(request:Request) {
+
+    const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
+    const produtos : TipoProduto[] = JSON.parse(file);
     
-    //Recebendo o request do cliente:
-    const{ nome, idade} = await request.json();
+    const {id, nome, preco, qtd} = await request.json();
 
-    console.log("NOME : ", nome);
-    console.log("IDADE : ", idade);
+    const produto = {id, nome, preco, qtd} as TipoProduto;
 
-    return NextResponse.json({msg:"SUCESSO"},{status:201});
+    produto.id = (produtos[produtos.length - 1].id + 1)
+
+    produtos.push(produto)
+    await fs.writeFile(process.cwd() + '/src/data/base.json', JSON.stringify(produtos))
+
+    return NextResponse.json(produto,{status:201});
 
 }
--- aula15/src/app/loading.tsx
+++ aula15/src/app/loading.tsx
@@ -1,8 +1,10 @@
+import Spinner from "@/components/Spinner/Spinner";
 
 export default function Loading() {
   return (
     <div>
       <h1>Loading...</h1>
+      <Spinner/>
     </div>
   )
 }
